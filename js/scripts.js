a4fc8283eebcb7892f617c50773acb18d4c113a9
DaveeeKR <129747290+DaveeeKR@users.noreply.github.com>
2023-11-14 22:55:58 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
 const dropdownBtn = document.querySelector('.bi-list');
 const navList = document.querySelector('.nav-list');
 const blur = document.querySelector('header');
+const blurMain = document.querySelector('main');
 
 dropdownBtn.addEventListener('click', function () {
 	navList.classList.toggle('active');
 	blur.classList.toggle('active');
+	blurMain.classList.toggle('active');
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+	const nav = document.querySelector('.nav');
+
+	function addShadow() {
+		if (window.scrollY >= 250) {
+			nav.classList.add('shadow-bg');
+		} else {
+			nav.classList.remove('shadow-bg');
+		}
+	}
+	window.addEventListener('scroll', addShadow);
 });
